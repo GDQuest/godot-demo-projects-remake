063595b89c8f6b12c3f87148dff5d4de15047351
Nathan Lovato <nathan@gdquest.com>
2020-02-01 15:28:43 -0600

    Add pause menu and user interface theme
    The menu has two buttons to resume and quit the game, respectively.
    
    Closes #4

---
+++ platformer-2d/src/UserInterface/PauseMenu.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+onready var resume_button = $VBoxContainer/ResumeButton
+
+
+func _ready():
+	visible = false
+
+
+func open():
+	visible = true
+	resume_button.grab_focus()
+
+
+func _on_ResumeButton_pressed() -> void:
+	get_tree().paused = false
+	visible = false
+
+
+func _on_QuitButton_pressed() -> void:
+	get_tree().quit()
